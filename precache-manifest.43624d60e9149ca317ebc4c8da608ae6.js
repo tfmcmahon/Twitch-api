ab1b4a9b2327a72caf0a3a773edb29325d81b958
tfmcmahon <tfmcmahon@gmail.com>
2020-03-02 14:54:17 -0600

    Updates

---
+++ precache-manifest.43624d60e9149ca317ebc4c8da608ae6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8a0893f2f9ec2d91f36322e3bfa2c9",
+    "url": "/twitch-api/index.html"
+  },
+  {
+    "revision": "95ffd036f7ceace5e3a3",
+    "url": "/twitch-api/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a162b56473800602e7ec",
+    "url": "/twitch-api/static/js/2.796d9b25.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/twitch-api/static/js/2.796d9b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ffd036f7ceace5e3a3",
+    "url": "/twitch-api/static/js/main.891a4b84.chunk.js"
+  },
+  {
+    "revision": "73019d21ccec8d0b94b6",
+    "url": "/twitch-api/static/js/runtime-main.8bca2eee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twitch-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
