6639a4dd642981195ec36e6866d0c2d600b29f42
tfmcmahon <tfmcmahon@gmail.com>
2020-04-02 12:56:41 -0500

    game undefinted fix

--- src/components/cards/CardBoxArtComponent.js
+++ src/components/cards/CardBoxArtComponent.js
@@ -13,7 +13,7 @@ const CardBoxArt = (props) => {
     useEffect(() => {
         let game = topGames.filter(game => game.id === props.gameId)
         let user = topUsers.filter(user => user.id === props.userId)
-        if (topGames.length > 0 && topUsers.length > 0 && user.length > 0) { //game.length > 0 && user.length > 0
+        if (topGames.length > 0 && topUsers.length > 0 && user.length > 0 && game.length > 0) {
             let [{box_art_url}] = game
             let [{name}] = game
             let [{profile_image_url}] = user
